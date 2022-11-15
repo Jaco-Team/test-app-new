dea22bde7e27092408d8028610433e7f03383cb8
vito3315 <marusiaqwerti@yandex.ru>
2022-11-16 00:18:29 +0400

    111

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['sans-serif']
+})
+
 import '../styles/globals.scss'
 
 import '../styles/akcii.scss'
@@ -8,12 +15,9 @@ import '../styles/loginForm.scss'
 import '../styles/header.scss'
 import '../styles/footer.scss'
 
-import { Roboto } from '@next/font/google'
 
-const roboto = Roboto({
-  weight: ['100', '300', '400', '500', '700', '900'],
-  subsets: ['sans-serif']
-})
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} className={roboto.className} />
