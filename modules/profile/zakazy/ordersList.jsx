bc554da0b017de40f53332b6f23af7ef27c30c45
vito3315 <marusiaqwerti@yandex.ru>
2023-04-06 19:38:58 +0400

    page orders

--- modules/profile/zakazy/ordersList.jsx
+++ modules/profile/zakazy/ordersList.jsx
@@ -2,14 +2,47 @@ import React from 'react';
 
 import Typography from '@mui/material/Typography';
 
-export default React.memo(function PromoCard({item}){
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import OrderItem from './orderItem.jsx';
+
+export default React.memo(function OrdersList({year, is_first}){
   return (
-    <div className={'promoCard'}>
-      <Typography component="span" className={'title'}>Что-то дает при применении</Typography>
-      <Typography component="span" className={'text'}>{item.promo_text}</Typography>
-      <Typography component="span" className={'endText'}>До окончания срока осталось:</Typography>
-      <Typography component="span" className={'endDate'}>1 день</Typography>
-      <Typography component="span" className={'name'}>{item.promo_name}</Typography>
-    </div>   
+    <Accordion defaultExpanded={ is_first }>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        className={'blockTitle'}
+      >
+        <Typography component="span">{year.year}</Typography>
+      </AccordionSummary>
+      <AccordionDetails className={'blockTable'}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>статус заказа</TableCell>
+              <TableCell>номер</TableCell>
+              <TableCell>дата</TableCell>
+              <TableCell>время</TableCell>
+              <TableCell>сумма</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {year.orders.map( (order, key) =>
+              <OrderItem key={key} order={order} />
+            )}
+          </TableBody>
+        </Table>
+      </AccordionDetails>
+    </Accordion> 
   )
 })
--- modules/profile/zakazy/page.jsx
+++ modules/profile/zakazy/page.jsx
@@ -3,24 +3,8 @@ import React, { useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-import { ReloadIcon, CheckIcon, CloseIcon, IconRuble } from '../../../ui/Icons.js';
-
 import ProfileBreadcrumbs from '../profileBreadcrumbs.jsx';
-import PromoCard from './orderItem.jsx';
+import OrdersList from './ordersList.jsx';
 
 import Meta from '@/components/meta.js';
 
@@ -46,52 +30,9 @@ export default function ContactsPage(props){
           </Grid>
 
           <Grid item xs={12}>
-            
             { orderList.map( (year, ykey) =>
-              <Accordion key={ykey} defaultExpanded={ ykey == 0 ? true : false }>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  className={'blockTitle'}
-                >
-                  <Typography component="span">{year.year}</Typography>
-                </AccordionSummary>
-                <AccordionDetails className={'blockTable'}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>статус заказа</TableCell>
-                        <TableCell>номер</TableCell>
-                        <TableCell>дата</TableCell>
-                        <TableCell>время</TableCell>
-                        <TableCell>сумма</TableCell>
-                        <TableCell></TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {year.orders.map( (order, key) =>
-                        <TableRow key={key} >
-                          <TableCell> 
-                            <div>
-                              { parseInt(order.is_delete) == 1 ? <CloseIcon /> : parseInt(order.type_status) == 5 ? <CheckIcon /> : <ReloadIcon /> }  {order.status_order}
-                            </div>
-                          </TableCell>
-                          <TableCell>{order.order_id}</TableCell>
-                          <TableCell>{order.date}</TableCell>
-                          <TableCell>{order.time}</TableCell>
-                          <TableCell>
-                            { new Intl.NumberFormat('ru-RU').format(order.sum)} ₽
-                          </TableCell>
-                          <TableCell>
-                            <span>Открыть</span>
-                          </TableCell>
-                        </TableRow>
-                      )}
-                    </TableBody>
-                  </Table>
-                </AccordionDetails>
-              </Accordion>
+              <OrdersList key={ykey} is_first={ykey == 0 ? true : false} year={year} />
             ) }
-
           </Grid>
 
         </Grid>
