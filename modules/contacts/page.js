d2e60df3cee698ba0ec99fdd54820606f372ea54
Denis <adadenis1982@yandex.ru>
2023-04-26 15:59:36 +0300

    contacts

--- modules/contacts/page.js
+++ modules/contacts/page.js
@@ -1,164 +1,156 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-import Script from 'next/script'
+import Script from 'next/script';
+import Link from 'next/link';
 
-import Grid from '@mui/material/Grid';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-import { useContactStore } from '@/components/store.js';
+import { useContactStore, useCitiesStore, useHeaderStore } from '@/components/store.js';
+import { shallow } from 'zustand/shallow';
 
 import Meta from '@/components/meta.js';
+import { MapPointIcon } from '@/ui/Icons.js';
 
-function ControlledAccordions(props) {
-  const [expanded, setExpanded] = React.useState(false);
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import List from '@mui/material/List';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import Divider from '@mui/material/Divider';
 
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
+import { styled } from '@mui/material/styles';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
-  const points = props.points;
+const IOSSwitch = styled((props) => (<Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />))(({ theme }) => ({
+  width: 70,
+  height: 30,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(40px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#57DC35',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color:
+        theme.palette.mode === 'light'
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 26,
+    height: 26,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 36 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
 
-  return (
-    <div className="Accordion">
-      {points.map((item, key) => 
-        <Accordion key={key} expanded={expanded === 'panel'+key} onChange={handleChange('panel'+key)}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            id={"panel"+key+"bh-header"}
-          >
-            <Typography variant="h5" component="span">{item.raion}: {item.addr}</Typography>
-          </AccordionSummary>
-          <AccordionDetails className="AccordionDesc" style={{ flexDirection: 'column', padding: 0 }}>
-            <div>
-              <Typography variant="h5" component="b">Название организации: </Typography>
-              <Typography variant="h5" component="span">{item.organization}</Typography>
-            </div>
-            <div>
-              <Typography variant="h5" component="b">ИНН / КПП: </Typography>
-              <Typography variant="h5" component="span">{item.inn} / {item.kpp}</Typography>
-            </div>
-            <div>
-              <Typography variant="h5" component="b">ОГРН: </Typography>
-              <Typography variant="h5" component="span">{item.ogrn}</Typography>
-            </div>
-            <div>
-              <Typography variant="h5" component="b">Фактический адрес: </Typography>
-              <Typography variant="h5" component="span">{item.full_addr}</Typography>
-            </div>
-            <div>
-              <Typography variant="h5" component="b">Телефон: </Typography>
-              <Typography variant="h5" component="span">{item.phone}</Typography>
-            </div>
-          </AccordionDetails>
-        </Accordion>
-      )}
-    </div>
-  );
-}
-
-function loadMap(points, points_zone){
-  let myMap2;
-  
-  ymaps.ready(function () {
-
-    myMap2 = new ymaps.Map('ForMap', {
-      center: [ points[0]['xy_center_map']['latitude'], points[0]['xy_center_map']['longitude'] ],
-      zoom: 10.8
-    });
-      
-    let HintLayout = ymaps.templateLayoutFactory.createClass( 
-      "<div class='my-hint'>" +
-          "<b>{{ properties.address }}</b><br />" +
-          "Зона {{ properties.zone }}<br />" +
-          "График работы: c 10:00 до 21:30<br />" +
-          "Стоимость доставки: {{ properties.sum_div }} руб." +
-      "</div>"
-    );
-
-    points_zone.map((zone, key)=>{
-      myMap2.geoObjects.add(
-        new ymaps.Polygon([zone], {
-          address: points[ key ]['addr'], 
-          sum_div: points[ key ]['sum_div'], 
-          zone: points[ key ]['test'],
-        }, {
-          hintLayout: HintLayout,
-          fillColor: 'rgba(187, 0, 37, 0.25)',
-          strokeColor: 'rgb(187, 0, 37)',
-          strokeWidth: 5
-        })
-      );
-    })
-      
-    points.map(function(point){
-      myMap2.geoObjects.add(
-        new ymaps.Placemark( [point['xy_point']['latitude'], point['xy_point']['longitude']], 
-        {}, 
-        {
-          iconLayout: 'default#image',
-          iconImageHref: '/Favikon.png',
-          iconImageSize: [30, 30],
-          iconImageOffset: [-12, -24],
-          iconContentOffset: [15, 15],
-        })
-      )
-    })
-  })
-}
-
-export default function ContactsPage(props){
-
+export default function ContactsPage(props) {
   const { page } = props;
 
-  const [ points, setPoints ] = useState([]);
-  const [ unicPoint, setUnicPoints ] = useState([]);
+  const [thisCityList, thisCityRu, setThisCityRu] = useCitiesStore((state) => [state.thisCityList, state.thisCityRu, state.setThisCityRu], shallow);
 
-  let [ myPoints, myUnicPoint, pointsZone ] = useContactStore((state) => [state.myPoints, state.myUnicPoint, state.pointsZone])
+  const [getData, myAddr, phone, disablePointsZone, disable, chooseAddr] = useContactStore((state) => [state.getData, state.myAddr, state.phone, state.disablePointsZone, state.disable, state.chooseAddr], shallow);
 
-  useEffect(() => {
-    
-    setUnicPoints(myUnicPoint);
-    setPoints( myPoints );
+  const [activePage] = useHeaderStore((state) => [state.activePage], shallow);
 
-    setTimeout(() => {
-      if( myPoints.length > 0 ){
-        loadMap(myPoints, pointsZone);
-      }
-    }, 500);
-  }, [myPoints, myUnicPoint, pointsZone]);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const open = Boolean(anchorEl);
+
+  const openMenu = (event) => setAnchorEl(event.currentTarget);
+
+  const chooseCity = (id) => {
+    const city = thisCityList.find((city) => city.id === id);
+
+    setThisCityRu(city.name);
+
+    setAnchorEl(null);
+
+    getData(activePage, city.link);
+  };
 
   return (
     <Meta title={page.title} description={page.description}>
-      <Grid container spacing={3} className="Contact mainContainer">
-                    
-        <Script src="https://api-maps.yandex.ru/2.1/?apikey=ae2bad1f-486e-442b-a9f7-d84fff6296db&lang=ru_RU" />
+      <Script src="https://api-maps.yandex.ru/2.1/?apikey=ae2bad1f-486e-442b-a9f7-d84fff6296db&lang=ru_RU" />
 
-        <Grid item xs={12}>
-          <Typography variant="h5" component="h1">Контакты</Typography>
-        </Grid>
-        
-        <Grid item lg={4} md={4} xl={4} sm={12} xs={12} className="mainContainer">
-          <Typography variant="h5" component="h2">Режим работы</Typography>
-          <Typography variant="h5" component="span" className="p20">Работаем ежедневно с 10:00 до 21:30</Typography>
-          <Typography variant="h5" component="h2">Телефон контакт-центра:</Typography>
+      {!myAddr.length ? null : 
+      <Grid item lg={4} md={4} xl={4} sm={12} xs={12} className="Contact">
 
-          {points[0] ?
-            <Typography variant="h5" component="a" className="p20" href={'tel:'+points[0].phone_new}>{points[0].phone}</Typography>
-              :
-            null
-          }
+        <Button className='chooseCity' onClick={openMenu} endIcon={<KeyboardArrowDownIcon />}>
+          <Typography variant="h5" component="span">{thisCityRu}</Typography>
+        </Button>
 
+        <Menu id='chooseCityContact' anchorEl={anchorEl} open={open} onClose={() => setAnchorEl(null)}>
+          {thisCityList.map((city, key) => (
+            <MenuItem key={key}>
+              <Link href={`/${city.link}/${activePage}`} onClick={() => chooseCity(city.id)}>
+                <span>{city.name}</span>
+              </Link>
+            </MenuItem>
+          ))}
+        </Menu>
+
+        <Grid item xs={12} sm={12} className="listAddrPoint">
           <Typography variant="h5" component="h2">Адреса кафе:</Typography>
-          <ControlledAccordions points={unicPoint}/>
+          <List>
+            {myAddr.map((point, key) => (
+              <ListItemButton key={key} disableRipple={false} onClick={() => chooseAddr(point.id)}>
+                <MapPointIcon style={{ width: '2vw', height: '2vw' }} />
+                <ListItemText primary={<Typography style={{ color: point?.color ? point.color ? point.color : null : null }}>{point.addr}</Typography>}/>
+              </ListItemButton>
+            ))}
+          </List>
+          <Divider style={{ marginTop: 20, marginBottom: 40 }} />
         </Grid>
-        <Grid item lg={8} md={8} xl={8} sm={12} xs={12} id="ForMap">
-          <div style={{ width: '100%', height: '100%', marginRight: 12, backgroundColor: '#e5e5e5' }} />    
+
+        <Grid item xs={12} sm={12} className="listInfo">
+          <Typography variant="h5" component="span">Телефон для заказа</Typography>
+          <Typography variant="h5" component="h2">{phone}</Typography>
+          <Typography variant="h5" component="span">Работаем ежедневно</Typography>
+          <Typography variant="h5" component="h2">с 10:00 до 21:30</Typography>
+          <Divider style={{ marginTop: 40, marginBottom: 40 }} />
         </Grid>
+
+        <Grid item xs={12} sm={12} className="switch">
+          <Typography variant="h5" component="span">Показать зону доставки</Typography>
+          <FormControlLabel control={<IOSSwitch checked={disable} onClick={disablePointsZone}/>}/>
+        </Grid>
+
+      </Grid>
+      }
+
+      <Grid item lg={8} md={8} xl={8} sm={12} xs={12} id="ForMap">
+        <div style={{ width: '100%', height: '100%', marginRight: 12, backgroundColor: '#e5e5e5' }}/>
       </Grid>
     </Meta>
-  )
+  );
 }
