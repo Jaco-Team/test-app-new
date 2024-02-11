08fcead883d79f08cf42b2f73303a1d65f82bea2
vito3315 <marusiaqwerti@yandex.ru>
2024-02-11 22:16:38 +0400

    111

--- components/header.js
+++ components/header.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 
-import Script from 'next/script';
-
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 import { roboto } from '../ui/Font.js';
@@ -44,8 +42,6 @@ export default React.memo(function Header({ city, city_list, cats, active_page }
 
   return (
     <div className={roboto.variable} style={{ overflow: 'auto' }}>
-
-      <Script src="https://yookassa.ru/checkout-widget/v1/checkout-widget.js" strategy="lazyOnload" />
 
       {matches ?
         <>
