19b506c55cd989999709224a6a0d177ea20f4e7c
vito3315 <marusiaqwerti@yandex.ru>
2024-03-31 22:23:11 +0400

    111

--- modules/home/menuCatMobile/menuCatMobile.js
+++ modules/home/menuCatMobile/menuCatMobile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHeaderStore, useHomeStore } from '@/components/store.js';
 import { Link as ScrollLink } from 'react-scroll';
 import Box from '@mui/material/Box';
@@ -8,46 +8,136 @@ import useCheckCat from '../hooks';
 import * as Scroll from 'react-scroll';
 var scroller = Scroll.scroller;
 
-const ChooseCat1 = memo( ({ category, offset, chooseCat }) => {
-  return(
-    <div className="menuCat" style={{ marginBottom: '1.7094017094017vw' }}>
-      {category.map((item, key) => (
-        <a
-          key={item.id}
-          className={'Cat'}
-          //to={'cat' + item.id}
-          id={'link_' + item.id}
-          //spy={true}
-          //isDynamic={true}
-          //smooth={false}
-          //offset={offset}
-          onClick={() => chooseCat(item.id, 'scroll') }
-          //onSetActive={() => chooseCat(item.id, null)}
-        >
-          <span>{item.name}</span>
-        </a>
-      ))}
-    </div>
-  )
-} );
-
-function areEqual(prevProps, nextProps) {
-  return JSON.stringify(prevProps.category) === JSON.stringify(nextProps.category);
-}
-
-export default memo(function MenuCatMobile({ city }) {
+export default function MenuCatMobile({ city }) {
 
   const [ category, setCategory ] = useHomeStore((state) => [ state.category, state.setCategory ]);
 
   const [catMenu, setCatMenu] = useState(category);
-  //const [catDopMenu, setCatDopMenu] = useState([]);
+  const [catDopMenu, setCatDopMenu] = useState([]);
   const [offset, setOffset] = useState(null);
 
-  //const [activePage] = useHeaderStore((state) => [state.activePage]);
+  let activeID = useCheckCat(category);
+  
+  useEffect(() => {
+    if( parseInt(activeID.id) !== parseInt(activeID.parent_id) ){
+      let chooseItem = catMenu.find( item => parseInt(item.id) == parseInt(activeID.parent_id) );
 
-  /**
-   * 
-   * {catDopMenu.length == 0 ? false : (
+      if( chooseItem ){
+        setCatDopMenu(chooseItem.cats);
+      }else{
+        setCatDopMenu([]);
+      }
+    }else{
+      setCatDopMenu([]);
+    }
+  }, [activeID]);
+
+  useEffect(() => {
+    setCatMenu(category);
+  }, [category]);
+
+  if (city == '') return null;
+
+  const [activePage] = useHeaderStore((state) => [state.activePage]);
+
+  // так оставить сброс состояния выбора категории товара при переходе на другие страницы ??
+  useEffect(() => {
+    if(activePage === 'home') {
+      setCatMenu(category)
+    } else {
+      const cat = category.map(item => item.choice = false)
+      setCatMenu(cat)
+    }
+  }, [activePage]);
+
+  const chooseCat = (id, scroll) => {
+    localStorage.setItem('goTo', id);
+
+    const menuCatDop = document.querySelector('.menuCatDop');
+
+    if (menuCatDop) {
+      menuCatDop.scrollLeft = 0;
+    }
+
+    const newCatMenu = catMenu.map((cat) => {
+      if (cat.id === id) {
+        cat.choice = true;
+
+        if(cat.cats.length > 0) {
+          cat.cats.map((cat) => (cat.choice = false));
+          setCatDopMenu(cat.cats);
+        } else {
+          setCatDopMenu([]);
+        }
+      } else {
+        cat.choice = false;
+      }
+
+      return cat;
+    });
+
+    setCatMenu(newCatMenu);
+
+    if (scroll) {
+      getScroll(id);
+    }
+  };
+
+  const chooseDopCat = (id, scroll) => {
+    localStorage.setItem('goTo', id);
+
+    const newCatDopMenu = catDopMenu.map((cat) => {
+      if (cat.id === id) {
+        cat.choice = true;
+      } else {
+        cat.choice = false;
+      }
+      return cat;
+    });
+
+    if (scroll) {
+      getScroll(id);
+    }
+  };
+
+  const getScroll = (id) => {
+    const header = document.querySelector('.headerMobile').getBoundingClientRect().height;
+
+    const menu = document.querySelector('.menuCatMobile').getBoundingClientRect().height;
+
+    const offset = -(header + menu);
+
+    setOffset(offset);
+
+    scroller.scrollTo('cat' + id, {
+      duration: 0,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+      offset,
+    });
+  };
+
+  return (
+    <Box sx={{ display: { xs: 'flex', md: 'flex', lg: 'none' } }} className="menuCatMobile">
+      <div className="menuCat" style={{ marginBottom: catDopMenu.length == 0 ? '1.7094017094017vw' : '2.5641025641026vw' }}>
+        {catMenu.map((item, key) => (
+          <ScrollLink
+            key={key}
+            className={'Cat'}
+            to={'cat' + item.id}
+            id={'link_' + item.id}
+            spy={true}
+            isDynamic={true}
+            smooth={false}
+            offset={offset}
+            onClick={() => chooseCat(item.id, 'scroll')}
+            //onSetActive={() => chooseCat(item.id, null)}
+          >
+            <span>{item.name}</span>
+          </ScrollLink>
+        ))}
+      </div>
+      {catDopMenu.length == 0 ? false : (
         <div className="menuCatDopContainer">
           <div className="menuCatDop" id="menuCatDop" >
             {catDopMenu.map((cat, key) => (
@@ -85,49 +175,9 @@ export default memo(function MenuCatMobile({ city }) {
           </div>
         </div>
       )}
-   */
 
-      const chooseCat = (id, scroll) => {
-        localStorage.setItem('goTo', id);
-    
-        const menuCatDop = document.querySelector('.menuCatDop');
-    
-        if (menuCatDop) {
-          //menuCatDop.scrollLeft = 0;
-        }
-    
-        const newCatMenu = catMenu.map((cat) => {
-          if (cat.id === id) {
-            cat.choice = true;
-    
-            if(cat.cats.length > 0) {
-              cat.cats.map((cat) => (cat.choice = false));
-              setCatDopMenu(cat.cats);
-            } else {
-              setCatDopMenu([]);
-            }
-          } else {
-            cat.choice = false;
-          }
-    
-          return cat;
-        });
-    
-        setCatMenu(newCatMenu);
-    
-        if (scroll) {
-          //getScroll(id);
-        }
-      };
-
-  return (
-    <Box className="menuCatMobile">
-      <ChooseCat1 category={category} offset={offset} chooseCat={chooseCat} />
-      
-      
-
-      <div className="blockShadowMenuCatMobile" style={{ position: 'sticky', top: '31.5786vw' }} /> 
+      <div className="blockShadowMenuCatMobile" style={{ position: 'sticky', top: catDopMenu.length != 0 ? '43.735042735043vw' : '31.5786vw' }} /> 
       
     </Box>
   );
-})
+}
