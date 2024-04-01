85a038c2da6be3bc6c7ccec1daa62fbf433c2e34
vito3315 <marusiaqwerti@yandex.ru>
2024-04-01 23:09:20 +0400

    111

--- modules/home/menuCatMobile/menuCatMobile.js
+++ modules/home/menuCatMobile/menuCatMobile.js
@@ -78,9 +78,6 @@ export default function MenuCatMobile({ city }) {
 
     setCatMenu(newCatMenu);
 
-    document.querySelector('.Cat').classList.remove('active');
-    document.querySelector('#link_'+id).classList.add('active');
-
     if (scroll) {
       getScroll(id);
     }
