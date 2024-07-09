453a8c2f378e5e2c42ebf804314e409e971e1e8a
vito3315 <marusiaqwerti@yandex.ru>
2024-07-09 22:59:36 +0400

    111

--- pages/yaauth.js
+++ pages/yaauth.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 
+import Script from 'next/script'
+
 export default React.memo(function Yaauth() {
 
   useEffect(() => {
+    console.log( 'load' )
     YaSendSuggestToken(
       'https://jacofood.ru', 
       {
@@ -12,6 +15,9 @@ export default React.memo(function Yaauth() {
   }, []);
 
   return (
-    <></>
+    <>
+      <Script src="https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-with-polyfills-latest.js" />
+      <Script src="https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-token-with-polyfills-latest.js" />
+    </>
   )
 })
