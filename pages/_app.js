6da9f0232e944839fe62e1d5f322eb77543b8a8f
vito3315 <marusiaqwerti@yandex.ru>
2024-10-28 20:14:26 +0400

    111

--- pages/_app.js
+++ pages/_app.js
@@ -108,7 +108,7 @@ Sentry.init({
 
   // Alternatively, use `process.env.npm_package_version` for a dynamic release version
   // if your build tool supports it.
-  release: "site_new@1.1.11",
+  release: "site_new@1.1.12",
   allowUrls: [/https?:\/\/((cdn|www)\.)?jacofood\.ru/],
   integrations: [
     Sentry.browserTracingIntegration(),
@@ -181,8 +181,8 @@ function MyApp({ Component, pageProps: { ...pageProps } }) {
           t.async=!0,
           t.src="https://vk.com/js/api/openapi.js?162",
           t.onload=function(){
-            VK.Retargeting.Init("VK-RTRG-409134-7MvqQ"),
-            VK.Retargeting.Hit()
+            VK?.Retargeting?.Init("VK-RTRG-409134-7MvqQ"),
+            VK?.Retargeting?.Hit()
           },
           document.head.appendChild(t)
         }();
