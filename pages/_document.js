05d8463390b2dbe02a5f0a3b0997a24d17c50038
vito3315 <marusiaqwerti@yandex.ru>
2024-11-26 22:07:20 +0400

    111

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ import { roboto } from '@/ui/Font.js';
 import Document from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
+/*export default class MyDocument extends Document {
   static async getInitialProps( ctx ) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -25,6 +25,47 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+}*/
+
+export default class MyDocument extends Document {
+  static async getInitialProps( ctx ) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage
+ 
+    try {
+      // Run the React rendering logic synchronously
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // Useful for wrapping the whole react tree
+          enhanceApp: (App) => App,
+          // Useful for wrapping in a per-page basis
+          enhanceComponent: (Component) => Component,
+        })
+  
+      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+      const initialProps = await Document.getInitialProps(ctx)
+  
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+ 
+  render() {
+    return (
+      <Html lang="ru" data-scroll="0">
+      <Head />
+      
+      <body className={roboto.variable}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+    )
   }
 }
 
