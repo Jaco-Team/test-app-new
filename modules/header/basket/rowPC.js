15351183cce84161d89a41ce1f7c4a677f6af45d
vito3315 <marusiaqwerti@yandex.ru>
2024-12-25 21:48:24 +0400

    111

--- modules/header/basket/rowPC.js
+++ modules/header/basket/rowPC.js
@@ -63,7 +63,7 @@ export default memo(function RowPC({ item, count, last }) {
           {
             promoInfo?.status_promo && (item?.new_one_price || item?.disabled) ?
               <span className="spanPromo">
-                {item?.disabled ? 'В подарок за ' : null}{new Intl.NumberFormat('ru-RU').format(item?.all_price)}{' '}₽
+                {item?.disabled ? 'По акции за ' : null}{new Intl.NumberFormat('ru-RU').format(item?.all_price)}{' '}₽
               </span>
               : 
             null
