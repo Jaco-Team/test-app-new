5bdb609baec796d4431e2f0d7d01dda80ecb7854
vito3315 <marusiaqwerti@yandex.ru>
2025-02-27 22:54:49 +0400

    111

--- components/YandexMetrika.js
+++ components/YandexMetrika.js
@@ -39,7 +39,8 @@ function YandexMetrikaTag({
                 clickmap:${clickmap},
                 trackLinks:${trackLinks},
                 accurateTrackBounce:${accurateTrackBounce},
-                webvisor:${webvisor}
+                webvisor:${webvisor},
+                ecommerce:"dataLayer"
             });
       `,
       }}
