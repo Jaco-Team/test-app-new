9d5224fa31a1360b2a51b45f55b27c81fb091cc5
vito3315 <marusiaqwerti@yandex.ru>
2025-05-16 12:00:16 +0400

    111

--- pages/_app.js
+++ pages/_app.js
@@ -174,6 +174,29 @@ function MetricaTLT2(){
   )
 }
 
+function MetricaTLT3(){
+  return ( 
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            var _tmr = window._tmr || (window._tmr = []);
+            _tmr.push({id: "3646239", type: "pageView", start: (new Date()).getTime()});
+            (function (d, w, id) {
+              if (d.getElementById(id)) return;
+              var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+              ts.src = "https://top-fwz1.mail.ru/js/code.js";
+              var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+              if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+            })(document, window, "tmr-code");
+          `,
+        }}
+      />
+      <noscript><div><img src="https://top-fwz1.mail.ru/counter?id=3646239;js=na" style={{position: 'fixed', left: '-9999px'}} alt="Top.Mail.Ru" /></div></noscript>
+    </>
+  )
+}
+
 function MetricaSMR(){
   return ( 
     <>
@@ -337,6 +360,7 @@ function MyApp({ Component, pageProps: { ...pageProps } }) {
 
       { pageProps?.data1?.city == 'togliatti' ? <MetricaTLT /> : null }
       { pageProps?.data1?.city == 'togliatti' ? <MetricaTLT2 /> : null }
+      { pageProps?.data1?.city == 'togliatti' ? <MetricaTLT3 /> : null }
       
       { pageProps?.data1?.city == 'samara' ? <MetricaSMR /> : null }
       { pageProps?.data1?.city == 'samara' ? <MetricaSMR2 /> : null }
