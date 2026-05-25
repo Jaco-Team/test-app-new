eae8b2d82af71f9657e82b79e54b578093c795ce
vito3315 <marusiaqwerti@yandex.ru>
2026-05-25 09:40:01 +0400

    111

--- modules/profile/profile/profileMobile.js
+++ modules/profile/profile/profileMobile.js
@@ -28,7 +28,15 @@ function CustomActionBar(props) {
   const { onAccept, onCancel, actions, className } = props;
   //const { onAccept, onClear, onCancel, onSetToday, actions, className } = props;
   return (
-    <DialogActions className={className} style={{ paddingBottom: '7vw', paddingLeft: '7vw', paddingRight: '7vw', flexDirection: 'column' }}>
+    <DialogActions
+      className={className}
+      style={{
+        paddingBottom: '7vw',
+        paddingLeft: '7vw',
+        paddingRight: '7vw',
+        flexDirection: 'column',
+      }}
+    >
       {actions.includes('cancel') && (
         <Button
           onClick={onCancel}
@@ -70,9 +78,16 @@ export default function ProfileMobile({ city, this_module }) {
 
   //const [userDate, setUserDate] = useState('');
   const [isSpam, setIsSpam] = useState(0);
-  
-  const [setActiveProfileModal, setUser, userInfo, getUserInfo, updateUser] = useProfileStore((state) => [state.setActiveProfileModal, state.setUser, state.userInfo, state.getUserInfo, state.updateUser]);
-  const [token] = useHeaderStoreNew( state => [ state?.token] )
+
+  const [setActiveProfileModal, setUser, userInfo, getUserInfo, updateUser] =
+    useProfileStore((state) => [
+      state.setActiveProfileModal,
+      state.setUser,
+      state.userInfo,
+      state.getUserInfo,
+      state.updateUser,
+    ]);
+  const [token] = useHeaderStoreNew((state) => [state?.token]);
 
   const { control, getValues, setValue } = useForm({
     defaultValues: {
@@ -84,25 +99,31 @@ export default function ProfileMobile({ city, this_module }) {
   });
 
   const now = new Date();
-  const minDate = new Date(now.getFullYear() - 100, now.getMonth(), now.getDate());
-  const maxDate = new Date(now.getFullYear() - 10, now.getMonth(), now.getDate());
+  const minDate = new Date(
+    now.getFullYear() - 100,
+    now.getMonth(),
+    now.getDate()
+  );
+  const maxDate = new Date(
+    now.getFullYear() - 10,
+    now.getMonth(),
+    now.getDate()
+  );
 
   useEffect(() => {
-
     setValue('name', userInfo?.name ?? '');
     // setValue('fam', userInfo?.fam ?? '');
     setValue('mail', userInfo?.mail ?? '');
     setIsSpam(userInfo?.spam);
     //setUserDate( userInfo?.date_bir?.length ? userInfo?.date_bir.toLowerCase() : '');
 
-    if( userInfo?.date_bir_full ){
-      setDate( parseISO(userInfo?.date_bir_full) );
+    if (userInfo?.date_bir_full) {
+      setDate(parseISO(userInfo?.date_bir_full));
     }
-
   }, [setValue, userInfo]);
 
   useEffect(() => {
-    if( token && token?.length > 0 ) {
+    if (token && token?.length > 0) {
       getUserInfo(this_module, city, token);
     }
   }, [token, city]);
@@ -119,24 +140,24 @@ export default function ProfileMobile({ city, this_module }) {
     updateUser(this_module, city, token);
   }
 
-  function changeOtherData(type, data){
-    userInfo[ [type] ] = data === true ? 1 : 0;
+  function changeOtherData(type, data) {
+    userInfo[[type]] = data === true ? 1 : 0;
 
     setUser(userInfo);
 
     setIsSpam(data === true ? 1 : 0);
 
     updateUser(this_module, city, token);
-  } 
+  }
 
-  function setUserDate(date, userInfo){
-    let newdate = dayjs(date).format('YYYY-MM-DD')
-    
+  function setUserDate(date, userInfo) {
+    let newdate = dayjs(date).format('YYYY-MM-DD');
+
     const [year, month, day] = newdate.split('-');
 
-    userInfo[ 'date_bir_d' ] = day;
-    userInfo[ 'date_bir_m' ] = month;
-    userInfo[ 'date_bir_y' ] = year;
+    userInfo['date_bir_d'] = day;
+    userInfo['date_bir_m'] = month;
+    userInfo['date_bir_y'] = year;
 
     userInfo.date_bir = newdate;
 
@@ -158,19 +179,22 @@ export default function ProfileMobile({ city, this_module }) {
         </div>
   */
 
-      //   <span className={userDate ? 'itemSpan' : 'itemSpan notDate'}>
-      //   {userDate ? userDate : 'Дата рождения'}
-      // </span>
-      // {userDate ? (
-      //   <LockMobile />
-      // ) : (
-      //   <div className="dateText">Подарим промокод ко дню рождения</div>
-      // )}
+  //   <span className={userDate ? 'itemSpan' : 'itemSpan notDate'}>
+  //   {userDate ? userDate : 'Дата рождения'}
+  // </span>
+  // {userDate ? (
+  //   <LockMobile />
+  // ) : (
+  //   <div className="dateText">Подарим промокод ко дню рождения</div>
+  // )}
 
   //onClick={() => userDate ? /*setActiveProfileModal(true, 'date_change')*/ () => {} : setActiveProfileModal(true, 'date_first')}
 
   return (
-    <Box sx={{ display: { xs: 'flex', md: 'flex', lg: 'none' } }} className="ProfileMobile">
+    <Box
+      sx={{ display: { xs: 'flex', md: 'flex', lg: 'none' } }}
+      className="ProfileMobile"
+    >
       <div className="profileLogin">
         <Link href={'/' + city + '/account'}>
           <ArrowLeftMobile />
@@ -209,19 +233,21 @@ export default function ProfileMobile({ city, this_module }) {
           />
           <EditPencilMobile />
         </div> */}
-        <div className="dataItem profileMain" /*onClick={() => setActiveProfileModal(true, 'phone')}*/>
+        <div
+          className="dataItem profileMain" /*onClick={() => setActiveProfileModal(true, 'phone')}*/
+        >
           <span className="itemSpan">{userInfo?.login ?? ''}</span>
           <LockMobile />
         </div>
         <div className="dataItem_ profileMain_">
-          <LocalizationProvider 
-            dateAdapter={AdapterDateFns} 
+          <LocalizationProvider
+            dateAdapter={AdapterDateFns}
             adapterLocale={ru}
             localeText={{
               cancelButtonLabel: 'Отмена',
               okButtonLabel: 'Выбрать',
               todayButtonLabel: 'Сегодня',
-              datePickerToolbarTitle: 'Выбрать дату'
+              datePickerToolbarTitle: 'Выбрать дату',
             }}
           >
             <MobileDatePicker
@@ -231,8 +257,8 @@ export default function ProfileMobile({ city, this_module }) {
               inputFormat="d MMMM yyyy"
               minDate={minDate}
               maxDate={maxDate}
-              disabled={ userInfo?.date_bir_full ? true : false }
-              onAccept={ (newValue) => setUserDate(newValue, userInfo) }
+              disabled={userInfo?.date_bir_full ? true : false}
+              onAccept={(newValue) => setUserDate(newValue, userInfo)}
               shouldRespectLeadingZeros={true}
               onClose={() => {
                 document.activeElement.blur();
@@ -252,11 +278,11 @@ export default function ProfileMobile({ city, this_module }) {
                   sx: {
                     '& span': {
                       textAlign: 'center',
-                      width: '100%'
+                      width: '100%',
                     },
                     '& h4': {
                       textAlign: 'center',
-                      width: '100%'
+                      width: '100%',
                     },
                   },
                 },
@@ -267,7 +293,7 @@ export default function ProfileMobile({ city, this_module }) {
                   },
                 },
                 textField: {
-                  placeholder: "День рождения", // Добавляем placeholder
+                  placeholder: 'День рождения', // Добавляем placeholder
                   sx: {
                     '& .MuiOutlinedInput-root': {
                       '& fieldset': {
@@ -283,11 +309,10 @@ export default function ProfileMobile({ city, this_module }) {
                     '& .MuiOutlinedInput-input': {
                       fontWeight: 500,
                       userSelect: 'none',
-                      WebkitUserSelect: 'none',  // Safari
-                      MozUserSelect: 'none',     // Firefox
+                      WebkitUserSelect: 'none', // Safari
+                      MozUserSelect: 'none', // Firefox
                       msUserSelect: 'none',
-                    }
-                    
+                    },
                   },
                 },
                 day: {
@@ -306,8 +331,11 @@ export default function ProfileMobile({ city, this_module }) {
               }}
             />
           </LocalizationProvider>
-          { !userInfo?.date_bir_full ? <div className="dateText">Подарим промокод ко дню рождения</div> : <LockMobile /> }
-          
+          {!userInfo?.date_bir_full ? (
+            <div className="dateText">Подарим промокод ко дню рождения</div>
+          ) : (
+            <LockMobile />
+          )}
         </div>
         <div className="dataItem profileMain">
           <Controller
@@ -327,17 +355,31 @@ export default function ProfileMobile({ city, this_module }) {
           <EditPencilMobile />
         </div>
         <div className="dataItem profileMain">
-          <span className="itemSwitch" style={{ color: parseInt(userInfo?.spam) === 1 ? 'rgba(0, 0, 0, 0.80)' : 'rgba(0, 0, 0, 0.40)' }}>
-            Хочу получать СМС с акциями, скидками и подарками
+          <span
+            className="itemSwitch"
+            style={{
+              color:
+                parseInt(userInfo?.spam) === 1
+                  ? 'rgba(0, 0, 0, 0.80)'
+                  : 'rgba(0, 0, 0, 0.40)',
+            }}
+          >
+            Хочу получать рассылки с акциями, скидками и подарками
           </span>
           <MySwitch
-            checked={parseInt(isSpam) == 1 ? true : false} onClick={ event => { changeOtherData('spam', event.target.checked) } }
+            checked={parseInt(isSpam) == 1 ? true : false}
+            onClick={(event) => {
+              changeOtherData('spam', event.target.checked);
+            }}
           />
         </div>
-        
       </div>
 
-      <ProfileModalMobile city={city} this_module={this_module} setUserDate={setUserDate} />
+      <ProfileModalMobile
+        city={city}
+        this_module={this_module}
+        setUserDate={setUserDate}
+      />
       <AccountModalMobile />
     </Box>
   );
--- modules/profile/profile/profilePC.jsx
+++ modules/profile/profile/profilePC.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
-import Link from 'next/link'
+import Link from 'next/link';
 
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller } from 'react-hook-form';
 
 import ProfileBreadcrumbs from '../profileBreadcrumbs.jsx';
 
@@ -25,7 +25,6 @@ import ModalAddr from './modalAddr.jsx';
 
 import { useProfileStore, useHeaderStoreNew } from '@/components/store.js';
 
-
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 import { ru } from 'date-fns/locale';
@@ -40,7 +39,15 @@ function CustomActionBar(props) {
   const { onAccept, onCancel, actions, className } = props;
   //const { onAccept, onClear, onCancel, onSetToday, actions, className } = props;
   return (
-    <DialogActions className={className} style={{ justifyContent: 'space-between', paddingBottom: '1.8vw', paddingLeft: '1.8vw', paddingRight: '1.8vw' }}>
+    <DialogActions
+      className={className}
+      style={{
+        justifyContent: 'space-between',
+        paddingBottom: '1.8vw',
+        paddingLeft: '1.8vw',
+        paddingRight: '1.8vw',
+      }}
+    >
       {actions.includes('cancel') && (
         <Button
           onClick={onCancel}
@@ -75,45 +82,66 @@ function CustomActionBar(props) {
   );
 }
 
-export default function ProfilePC({ page, this_module, city }){
+export default function ProfilePC({ page, this_module, city }) {
   const [date, setDate] = useState();
 
-  const [ getUserInfo, setUser, userInfo, streets, shortName, updateUser, openModalAddr, delAddr ] = useProfileStore( state => [ state.getUserInfo, state.setUser, state.userInfo, state.streets, state.shortName, state.updateUser, state.openModalAddr, state.delAddr ] );
+  const [
+    getUserInfo,
+    setUser,
+    userInfo,
+    streets,
+    shortName,
+    updateUser,
+    openModalAddr,
+    delAddr,
+  ] = useProfileStore((state) => [
+    state.getUserInfo,
+    state.setUser,
+    state.userInfo,
+    state.streets,
+    state.shortName,
+    state.updateUser,
+    state.openModalAddr,
+    state.delAddr,
+  ]);
 
-  const [ token, signOut ] = useHeaderStoreNew( state => [ state?.token, state?.signOut ] )
+  const [token, signOut] = useHeaderStoreNew((state) => [
+    state?.token,
+    state?.signOut,
+  ]);
 
   const { control, getValues, setValue } = useForm({
     defaultValues: {
       name: '',
       // fam: '',
       login: '',
-      mail: ''
-    }
+      mail: '',
+    },
   });
 
-  const [ isSpam, setIsSpam ] = useState(0);
+  const [isSpam, setIsSpam] = useState(0);
 
-  const [ user_d, setUser_d ] = useState('');
-  const [ user_m, setUser_m ] = useState('');
+  const [user_d, setUser_d] = useState('');
+  const [user_m, setUser_m] = useState('');
 
-  const [ arr_d, setArr_d ] = useState([]);
-  const [ arr_m, setArr_m ] = useState([ 
-    {name: 'Января', id: 1},
-    {name: 'Февраля', id: 2},
-    {name: 'Марта', id: 3},
-    {name: 'Апреля', id: 4},
-    {name: 'Мая', id: 5},
-    {name: 'Июня', id: 6},
-    {name: 'Июля', id: 7},
-    {name: 'Августа', id: 8},
-    {name: 'Сентября', id: 9},
-    {name: 'Октября', id: 10},
-    {name: 'Ноября', id: 11},
-    {name: 'Декабря', id: 12}
+  const [arr_d, setArr_d] = useState([]);
+  const [arr_m, setArr_m] = useState([
+    { name: 'Января', id: 1 },
+    { name: 'Февраля', id: 2 },
+    { name: 'Марта', id: 3 },
+    { name: 'Апреля', id: 4 },
+    { name: 'Мая', id: 5 },
+    { name: 'Июня', id: 6 },
+    { name: 'Июля', id: 7 },
+    { name: 'Августа', id: 8 },
+    { name: 'Сентября', id: 9 },
+    { name: 'Октября', id: 10 },
+    { name: 'Ноября', id: 11 },
+    { name: 'Декабря', id: 12 },
   ]);
 
   // if( arr_d.length == 0 ){
-    
+
   //   let arr_d = [];
 
   //   for(let i = 1; i <= 31; i ++){
@@ -127,33 +155,39 @@ export default function ProfilePC({ page, this_module, city }){
   // }
 
   const now = new Date();
-  const minDate = new Date(now.getFullYear() - 100, now.getMonth(), now.getDate());
-  const maxDate = new Date(now.getFullYear() - 10, now.getMonth(), now.getDate());
+  const minDate = new Date(
+    now.getFullYear() - 100,
+    now.getMonth(),
+    now.getDate()
+  );
+  const maxDate = new Date(
+    now.getFullYear() - 10,
+    now.getMonth(),
+    now.getDate()
+  );
 
   useEffect(() => {
-    if( token && token.length > 0 ){
+    if (token && token.length > 0) {
       getUserInfo(this_module, city, token);
     }
   }, [token, city]);
 
   useEffect(() => {
-    setValue("name", userInfo?.name)
+    setValue('name', userInfo?.name);
     // setValue("fam", userInfo?.fam)
-    setValue("login", userInfo?.login)
-    setValue("mail", userInfo?.mail)
+    setValue('login', userInfo?.login);
+    setValue('mail', userInfo?.mail);
 
-    setIsSpam(userInfo?.spam)
-    setUser_d(userInfo?.date_bir_d)  
-    setUser_m(userInfo?.date_bir_m)
+    setIsSpam(userInfo?.spam);
+    setUser_d(userInfo?.date_bir_d);
+    setUser_m(userInfo?.date_bir_m);
 
-    if( userInfo?.date_bir_full ){
-      setDate( new Date(userInfo?.date_bir_full) );
+    if (userInfo?.date_bir_full) {
+      setDate(new Date(userInfo?.date_bir_full));
     }
-    
-
   }, [userInfo]);
 
-  function saveMainData(userInfo){
+  function saveMainData(userInfo) {
     let userData = getValues();
 
     userInfo.name = userData?.name;
@@ -165,42 +199,42 @@ export default function ProfilePC({ page, this_module, city }){
     updateUser(this_module, city, token);
   }
 
-  function changeOtherData(type, data, userInfo){
-    userInfo[ [type] ] = data === true ? 1 : 0;
+  function changeOtherData(type, data, userInfo) {
+    userInfo[[type]] = data === true ? 1 : 0;
 
     setUser(userInfo);
 
-    setIsSpam(data === true ? 1 : 0)
+    setIsSpam(data === true ? 1 : 0);
 
     updateUser(this_module, city, token);
-  }  
+  }
 
-  function changeUserData(data, value, userInfo){
-    userInfo[ [data] ] = value;
+  function changeUserData(data, value, userInfo) {
+    userInfo[[data]] = value;
 
     setUser(userInfo);
 
-    if( data == 'date_bir_d' ){
-      setUser_d(value)
+    if (data == 'date_bir_d') {
+      setUser_d(value);
     }
 
-    if( data == 'date_bir_m' ){
-      setUser_m(value)
+    if (data == 'date_bir_m') {
+      setUser_m(value);
     }
 
-    if( userInfo?.date_bir_m > 0 && userInfo?.date_bir_d > 0 ){
+    if (userInfo?.date_bir_m > 0 && userInfo?.date_bir_d > 0) {
       updateUser(this_module, city, token);
     }
   }
 
-  function setUserDate(date, userInfo){
-    let newdate = dayjs(date).format('YYYY-MM-DD')
-    
+  function setUserDate(date, userInfo) {
+    let newdate = dayjs(date).format('YYYY-MM-DD');
+
     const [year, month, day] = newdate.split('-');
 
-    userInfo[ 'date_bir_d' ] = day;
-    userInfo[ 'date_bir_m' ] = month;
-    userInfo[ 'date_bir_y' ] = year;
+    userInfo['date_bir_d'] = day;
+    userInfo['date_bir_m'] = month;
+    userInfo['date_bir_y'] = year;
 
     userInfo.date_bir = newdate;
 
@@ -208,10 +242,10 @@ export default function ProfilePC({ page, this_module, city }){
 
     updateUser(this_module, city, token);
   }
-  
+
   // <div>
   //             <div>
-  //               <MySelect 
+  //               <MySelect
   //                 data={arr_d}
   //                 className="date_d"
   //                 disabled={ userInfo?.date_bir_m > 0 && userInfo?.date_bir_d > 0 ? true : false }
@@ -220,7 +254,7 @@ export default function ProfilePC({ page, this_module, city }){
   //               />
   //             </div>
   //             <div>
-  //               <MySelect 
+  //               <MySelect
   //                 data={arr_m}
   //                 className="date_m"
   //                 disabled={ userInfo?.date_bir_m > 0 && userInfo?.date_bir_d > 0 ? true : false }
@@ -231,18 +265,22 @@ export default function ProfilePC({ page, this_module, city }){
   //           </div>
 
   return (
-    <Grid container spacing={3} className="profilePagePCWrap" style={{ margin: 0, width: '100%' }}>
+    <Grid
+      container
+      spacing={3}
+      className="profilePagePCWrap"
+      style={{ margin: 0, width: '100%' }}
+    >
       <Grid item className="Profile mainContainer">
-        
         <Grid item xs={12}>
-          <Typography variant="h5" component="h1">Личные данные</Typography>
+          <Typography variant="h5" component="h1">
+            Личные данные
+          </Typography>
         </Grid>
 
         <Grid item xs={12} className="main_data">
           <div>
-            <span>
-              {shortName ? shortName : <ProfileIconNew />}
-            </span>
+            <span>{shortName ? shortName : <ProfileIconNew />}</span>
           </div>
           <div>
             <div style={{ marginTop: '0.1vw' }}>
@@ -251,7 +289,7 @@ export default function ProfilePC({ page, this_module, city }){
                 render={({ field: { onChange, onBlur, value } }) => (
                   <MyTextInput
                     placeholder="Имя"
-                    onBlur={ () => saveMainData(userInfo) }
+                    onBlur={() => saveMainData(userInfo)}
                     func={onChange}
                     value={value}
                   />
@@ -294,7 +332,7 @@ export default function ProfilePC({ page, this_module, city }){
                 render={({ field: { onChange, onBlur, value } }) => (
                   <MyTextInput
                     placeholder="name@mail.ru"
-                    onBlur={ () => saveMainData(userInfo) }
+                    onBlur={() => saveMainData(userInfo)}
                     func={onChange}
                     value={value}
                   />
@@ -308,8 +346,13 @@ export default function ProfilePC({ page, this_module, city }){
         <Grid item xs={12} className="check_data">
           <div>
             <div>
-              <span>Хочу получать СМС с акциями и скидками</span>
-              <MySwitch checked={ parseInt(isSpam) == 1 ? true : false } onClick={ event => { changeOtherData('spam', event.target.checked, userInfo) } } />
+              <span>Хочу получать рассылки с акциями и скидками</span>
+              <MySwitch
+                checked={parseInt(isSpam) == 1 ? true : false}
+                onClick={(event) => {
+                  changeOtherData('spam', event.target.checked, userInfo);
+                }}
+              />
             </div>
             <div style={{ display: 'none' }}>
               <span>Хочу получать цифровые чеки на электронную почту</span>
@@ -329,14 +372,14 @@ export default function ProfilePC({ page, this_module, city }){
               <span>Дата вашего рождения</span>
             </div>
             <div>
-              <LocalizationProvider 
-                dateAdapter={AdapterDateFns} 
+              <LocalizationProvider
+                dateAdapter={AdapterDateFns}
                 adapterLocale={ru}
                 localeText={{
                   cancelButtonLabel: 'Отмена',
                   okButtonLabel: 'Выбрать',
                   todayButtonLabel: 'Сегодня',
-                  datePickerToolbarTitle: 'Выбрать дату'
+                  datePickerToolbarTitle: 'Выбрать дату',
                 }}
               >
                 <MobileDatePicker
@@ -345,8 +388,8 @@ export default function ProfilePC({ page, this_module, city }){
                   format="d MMMM yyyy"
                   minDate={minDate}
                   maxDate={maxDate}
-                  disabled={ userInfo?.date_bir_full ? true : false }
-                  onAccept={ (newValue) => setUserDate(newValue, userInfo) }
+                  disabled={userInfo?.date_bir_full ? true : false}
+                  onAccept={(newValue) => setUserDate(newValue, userInfo)}
                   //showToolbar         // Включаем верхнюю панель
                   //toolbarFormat="d MMMM yyyy"
                   slots={{
@@ -364,11 +407,11 @@ export default function ProfilePC({ page, this_module, city }){
                       sx: {
                         '& span': {
                           textAlign: 'center',
-                          width: '100%'
+                          width: '100%',
                         },
                         '& h4': {
                           textAlign: 'center',
-                          width: '100%'
+                          width: '100%',
                         },
                       },
                     },
@@ -379,12 +422,12 @@ export default function ProfilePC({ page, this_module, city }){
                       },
                     },
                     textField: {
-                      placeholder: "Выберите дату", // Добавляем placeholder
+                      placeholder: 'Выберите дату', // Добавляем placeholder
                       sx: {
                         '& .MuiOutlinedInput-root': {
                           '& fieldset': {
                             borderWidth: '0.1083032491vw', // Толстый бордер
-                            borderColor: 'rgba(0, 0, 0, 0.23)',  // Цвет бордера
+                            borderColor: 'rgba(0, 0, 0, 0.23)', // Цвет бордера
                           },
                           borderRadius: '100px', // Скругление
                         },
@@ -407,9 +450,10 @@ export default function ProfilePC({ page, this_module, city }){
                 />
               </LocalizationProvider>
             </div>
-            
+
             <div>
-              Дату рождения можно выбрать только один раз. Будьте внимательны, так как изменить её позже не получится.
+              Дату рождения можно выбрать только один раз. Будьте внимательны,
+              так как изменить её позже не получится.
             </div>
           </div>
         </Grid>
@@ -418,40 +462,55 @@ export default function ProfilePC({ page, this_module, city }){
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell colSpan={3} className="headTable">Мои адреса</TableCell>
+                <TableCell colSpan={3} className="headTable">
+                  Мои адреса
+                </TableCell>
                 <TableCell colSpan={2} className="headAddTable">
-                  <div className="headAddTable__" onClick={ () => openModalAddr(0, city) }>
+                  <div
+                    className="headAddTable__"
+                    onClick={() => openModalAddr(0, city)}
+                  >
                     <span>Добавить</span>
                   </div>
                 </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              
-              {streets?.map( (item, key) =>
+              {streets?.map((item, key) => (
                 <TableRow key={key}>
                   <TableCell>{item.city_name}</TableCell>
-                  <TableCell>{item.name_street}, д. {item.home}, кв. {item.kv}</TableCell>
-                  <TableCell className={ parseInt(item.is_main) == 1 ? 'ChooseAddr' : '' }>{ parseInt(item.is_main) == 1 ? <div>Основной</div> : false }</TableCell>
-                  <TableCell className='ChangeAddr'><span onClick={ () => openModalAddr(item.id, item.city) }>Изменить</span></TableCell>
-                  <TableCell><CloseIconMin onClick={ () => delAddr(item.id, token) } /></TableCell>
-                </TableRow> 
-              )}
-            
+                  <TableCell>
+                    {item.name_street}, д. {item.home}, кв. {item.kv}
+                  </TableCell>
+                  <TableCell
+                    className={parseInt(item.is_main) == 1 ? 'ChooseAddr' : ''}
+                  >
+                    {parseInt(item.is_main) == 1 ? <div>Основной</div> : false}
+                  </TableCell>
+                  <TableCell className="ChangeAddr">
+                    <span onClick={() => openModalAddr(item.id, item.city)}>
+                      Изменить
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <CloseIconMin onClick={() => delAddr(item.id, token)} />
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </Grid>
 
         <Grid item xs={12} className="log_out">
           <span style={{ visibility: 'hidden' }}>Удалить аккаунт</span>
-          <Link href={"/"+city} onClick={ () => signOut(city) }>Выйти</Link>
+          <Link href={'/' + city} onClick={() => signOut(city)}>
+            Выйти
+          </Link>
         </Grid>
-
       </Grid>
-      
+
       <ProfileBreadcrumbs />
       <ModalAddr />
-
     </Grid>
-  )
+  );
 }
