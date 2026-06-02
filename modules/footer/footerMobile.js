9a25737388698bcd8141c2f98152feb176d1e2b4
vito3315 <marusiaqwerti@yandex.ru>
2026-06-02 19:05:48 +0400

    111

--- modules/footer/footerMobile.js
+++ modules/footer/footerMobile.js
@@ -3,9 +3,20 @@ import Link from 'next/link';
 import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
-import {useCartStore, useHomeStore, useProfileStore} from '@/components/store.js';
+import {
+  useCartStore,
+  useHomeStore,
+  useProfileStore,
+} from '@/components/store.js';
 
-import { NewVKIcon, OdnIcon, TGIcon, RutubeIcon, ArrowUp, BasketFooterMobile } from '@/ui/Icons.js';
+import {
+  NewVKIcon,
+  OdnIcon,
+  TGIcon,
+  RutubeIcon,
+  ArrowUp,
+  BasketFooterMobile,
+} from '@/ui/Icons.js';
 import { BREAKPOINTS } from '@/utils/breakpoints';
 import ModalOrderMobile from '@/modules/profile/zakazy/mobile/modalOrderMobile';
 
@@ -15,16 +26,32 @@ function tw(px) {
 }
 
 import { reachGoal } from '@/utils/metrika';
-import { getLocalStorageItem, setLocalStorageItem } from '@/utils/browserStorage';
+import {
+  getLocalStorageItem,
+  setLocalStorageItem,
+} from '@/utils/browserStorage';
 
 export default function FooterMobile({ cityName, active_page, links }) {
-
   const [cookie, setCookie] = useState(true);
   const [showArrow, setShowArrow] = useState(false);
-  
-  const [itemsCount, itemsOffDops, dopListCart, checkPromo, allPrice] = useCartStore((state) => [state.itemsCount, state.itemsOffDops, state.dopListCart, state.checkPromo, state.allPrice]);
 
-  const [setMenuCatPosition, isOpenFilter, transition_menu_mobile, badge_filter, tag_filter, text_filter] = useHomeStore(state => [
+  const [itemsCount, itemsOffDops, dopListCart, checkPromo, allPrice] =
+    useCartStore((state) => [
+      state.itemsCount,
+      state.itemsOffDops,
+      state.dopListCart,
+      state.checkPromo,
+      state.allPrice,
+    ]);
+
+  const [
+    setMenuCatPosition,
+    isOpenFilter,
+    transition_menu_mobile,
+    badge_filter,
+    tag_filter,
+    text_filter,
+  ] = useHomeStore((state) => [
     state.setMenuCatPosition,
     state.isOpenFilter,
     state.transition_menu_mobile,
@@ -36,7 +63,7 @@ export default function FooterMobile({ cityName, active_page, links }) {
   const isTabletLayout = useMediaQuery(
     `(min-width:${BREAKPOINTS.tabletMin}px) and (max-width:${BREAKPOINTS.tabletMax}px)`
   );
-  const [ saveUserActions ] = useProfileStore((state) => [state.saveUserActions]);
+  const [saveUserActions] = useProfileStore((state) => [state.saveUserActions]);
 
   const handlerArrow = () => {
     setShowArrow(window.scrollY > 50);
@@ -51,7 +78,7 @@ export default function FooterMobile({ cityName, active_page, links }) {
     if (currentCatPosition && window.scrollY < 180) {
       setMenuCatPosition(false);
     }
-  }
+  };
 
   const scrollUp = () =>
     window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
@@ -71,19 +98,29 @@ export default function FooterMobile({ cityName, active_page, links }) {
     if (!savedCookie?.length) setCookie(false);
   }, []);
 
-  let price1 = itemsOffDops.reduce((all, it) => parseInt(all) + parseInt(it.count) * parseInt(it.one_price), 0);
-  let price2 = dopListCart.reduce((all, it) => parseInt(all) + parseInt(it.count) * parseInt(it.one_price), 0);
-  
+  let price1 = itemsOffDops.reduce(
+    (all, it) => parseInt(all) + parseInt(it.count) * parseInt(it.one_price),
+    0
+  );
+  let price2 = dopListCart.reduce(
+    (all, it) => parseInt(all) + parseInt(it.count) * parseInt(it.one_price),
+    0
+  );
+
   let baseTotal = price1 + price2;
 
-  const totalToShow = (checkPromo?.st && itemsOffDops.length ? allPrice : null) ?? baseTotal;
+  const totalToShow =
+    (checkPromo?.st && itemsOffDops.length ? allPrice : null) ?? baseTotal;
 
-  const ext = (url) => url ? { href: url, target: '_blank', rel: 'noopener noreferrer' } : { href: '/#' };
+  const ext = (url) =>
+    url
+      ? { href: url, target: '_blank', rel: 'noopener noreferrer' }
+      : { href: '/#' };
 
   const openCart = () => {
     saveUserActions('open_card', '', 0);
-    reachGoal('open_basket'); 
-  }
+    reachGoal('open_basket');
+  };
 
   const stickyBarStyle = isTabletLayout
     ? {
@@ -97,8 +134,14 @@ export default function FooterMobile({ cityName, active_page, links }) {
       }
     : {
         bottom: cookie ? '3.4188034188034vw' : '37.094017094017vw',
-        width: itemsCount && active_page === 'home' ? '64.529914529915vw' : '10.25641025641vw',
-        left: itemsCount && active_page === 'home' ? '32.051282051282vw' : '86.324786324786vw',
+        width:
+          itemsCount && active_page === 'home'
+            ? '64.529914529915vw'
+            : '10.25641025641vw',
+        left:
+          itemsCount && active_page === 'home'
+            ? '32.051282051282vw'
+            : '86.324786324786vw',
         marginTop:
           active_page === 'home' && isOpenFilter
             ? transition_menu_mobile
@@ -112,10 +155,12 @@ export default function FooterMobile({ cityName, active_page, links }) {
         marginTop:
           active_page === 'sitemap' ||
           active_page === 'contacts' ||
-          (active_page === 'home' && (badge_filter || tag_filter || text_filter))
+          (active_page === 'home' &&
+            (badge_filter || tag_filter || text_filter))
             ? '-4.3321299638989vw'
             : undefined,
-        transform: active_page === 'contacts' ? 'translate(0, -50%)' : undefined,
+        transform:
+          active_page === 'contacts' ? 'translate(0, -50%)' : undefined,
       }
     : {
         marginTop:
@@ -141,84 +186,157 @@ export default function FooterMobile({ cityName, active_page, links }) {
           <Link
             onClick={openCart}
             href={'/' + cityName + '/cart'}
-            className={itemsCount && active_page === 'home' ? 'BasketFooterMobile' : 'BasketFooterMobileHidden'}
+            className={
+              itemsCount && active_page === 'home'
+                ? 'BasketFooterMobile'
+                : 'BasketFooterMobileHidden'
+            }
           >
-            <span><BasketFooterMobile /></span>
+            <span>
+              <BasketFooterMobile />
+            </span>
             <span>{new Intl.NumberFormat('ru-RU').format(totalToShow)} ₽</span>
           </Link>
         ) : null}
 
         <div
-          className={showArrow && active_page !== 'cart' ? 'ArrowMobile' : 'ArrowHidden'}
+          className={
+            showArrow && active_page !== 'cart' ? 'ArrowMobile' : 'ArrowHidden'
+          }
           onClick={scrollUp}
           style={arrowMarginStyle}
         >
           <ArrowUp />
         </div>
-
       </div>
-      
-      <footer className="footerMobile" style={{ minHeight: cookie ? '128.20512820513vw' : '165.29914529915vw' }}>
+
+      <footer
+        className="footerMobile"
+        style={{
+          minHeight: cookie ? '128.20512820513vw' : '165.29914529915vw',
+        }}
+      >
         <div
           className="ContainerMobileFooter"
           ref={(node) => {
             if (node && cookie) {
-              node.style.setProperty('margin-bottom', '13.675213675214vw', 'important');
+              node.style.setProperty(
+                'margin-bottom',
+                '13.675213675214vw',
+                'important'
+              );
             }
           }}
         >
-          <div className="icon" role="navigation" aria-label="Мы в социальных сетях">
-            {Object.keys(links || {}).length === 0 ? false :
+          <div
+            className="icon"
+            role="navigation"
+            aria-label="Мы в социальных сетях"
+          >
+            {Object.keys(links || {}).length === 0 ? (
+              false
+            ) : (
               <>
-                <Link {...ext(links?.link_vk)} aria-label="Мы ВКонтакте"><NewVKIcon aria-hidden="true" focusable="false" /></Link>
-                <Link {...ext(links?.link_tg)} aria-label="Мы в Telegram"><TGIcon aria-hidden="true" focusable="false" /></Link>
-                <Link {...ext(links?.link_ok)} aria-label="Мы в Одноклассниках"><OdnIcon aria-hidden="true" focusable="false" /></Link>
+                <Link {...ext(links?.link_vk)} aria-label="Мы ВКонтакте">
+                  <NewVKIcon aria-hidden="true" focusable="false" />
+                </Link>
+                <Link {...ext(links?.link_tg)} aria-label="Мы в Telegram">
+                  <TGIcon aria-hidden="true" focusable="false" />
+                </Link>
+                <Link {...ext(links?.link_ok)} aria-label="Мы в Одноклассниках">
+                  <OdnIcon aria-hidden="true" focusable="false" />
+                </Link>
                 <Link {...ext(links?.link_rt)} aria-label="Мы в RuTube">
                   <RutubeIcon aria-hidden="true" focusable="false" />
                 </Link>
               </>
-            }
+            )}
           </div>
 
           <div className="row" style={{ marginBottom: '11.965811965812vw' }}>
-            <div className="column" style={{ marginRight: '2.991452991453vw', paddingLeft: '0.85470085470085vw', width: '36.752136752137vw' }}>
+            <div
+              className="column"
+              style={{
+                marginRight: '2.991452991453vw',
+                paddingLeft: '0.85470085470085vw',
+                width: '36.752136752137vw',
+              }}
+            >
               <Typography component="span">Жако</Typography>
               <Link href={'/' + cityName + '/about'}>О Компании</Link>
               <Link href={'/' + cityName + '/company-details'}>Реквизиты</Link>
-              <Link href={'/' + cityName + '/contacts'} style={{ marginBottom: 0 }}>
-                Контакты
+              <Link href={'/' + cityName + '/contacts'}>Контакты</Link>
+              <Link
+                href={'/' + cityName + '/dostavka'}
+                style={{ marginBottom: 0 }}
+              >
+                Доставка
               </Link>
             </div>
             <div className="column">
               <Typography component="span">Документы</Typography>
-              {Object.keys(links || {}).length === 0 ? false :
+              {Object.keys(links || {}).length === 0 ? (
+                false
+              ) : (
                 <Link {...ext(links?.link_allergens)}>
                   Калорийность, состав, БЖУ
                 </Link>
-              }
-              <Link href={'/' + cityName + '/publichnaya-oferta'}>Публичная оферта</Link>
-              <Link href={'/' + cityName + '/politika-konfidencialnosti'}>Политика конфиденциальности</Link>
-              <Link href={'/' + cityName + '/legal'}>Согласие на обработку персональных данных</Link>
-              <Link href={'/' + cityName + '/politika-legal'}>Политика в отношении обработки метрических данных</Link>
-              <Link href={'/' + cityName + '/instpayorders'}>Правила оплаты</Link>
-              <Link href={'/' + cityName + '/sitemap'} style={{ marginBottom: 0 }}>Карта сайта</Link>
+              )}
+              <Link href={'/' + cityName + '/publichnaya-oferta'}>
+                Публичная оферта
+              </Link>
+              <Link href={'/' + cityName + '/politika-konfidencialnosti'}>
+                Политика конфиденциальности
+              </Link>
+              <Link href={'/' + cityName + '/legal'}>
+                Согласие на обработку персональных данных
+              </Link>
+              <Link href={'/' + cityName + '/politika-legal'}>
+                Политика в отношении обработки метрических данных
+              </Link>
+              <Link href={'/' + cityName + '/instpayorders'}>
+                Правила оплаты
+              </Link>
+              <Link
+                href={'/' + cityName + '/sitemap'}
+                style={{ marginBottom: 0 }}
+              >
+                Карта сайта
+              </Link>
             </div>
           </div>
 
           <div className="row" style={{ marginBottom: '1.7094017094017vw' }}>
-            <div className="column" style={{ marginRight: '2.991452991453vw', paddingLeft: '0.85470085470085vw', width: '36.752136752137vw' }}>
+            <div
+              className="column"
+              style={{
+                marginRight: '2.991452991453vw',
+                paddingLeft: '0.85470085470085vw',
+                width: '36.752136752137vw',
+              }}
+            >
               <Typography component="span">Работа в жако</Typography>
               <Link href={'/' + cityName + '/jobs'}>Вакансии</Link>
             </div>
             <div className="column">
               <Typography component="span">Франшиза</Typography>
-              <Link {...ext('https://franchise.jacofood.ru')}>Сайт франшизы</Link>
-              <Link {...ext('https://invest.jacofood.ru')}>Сайт для инвестиций</Link>
+              <Link {...ext('https://franchise.jacofood.ru')}>
+                Сайт франшизы
+              </Link>
+              <Link {...ext('https://invest.jacofood.ru')}>
+                Сайт для инвестиций
+              </Link>
             </div>
           </div>
 
           <div className="copy">
-            <Typography component="span" className="copy" suppressHydrationWarning>{new Date().getFullYear()} © Жако</Typography>
+            <Typography
+              component="span"
+              className="copy"
+              suppressHydrationWarning
+            >
+              {new Date().getFullYear()} © Жако
+            </Typography>
           </div>
         </div>
 
@@ -227,8 +345,14 @@ export default function FooterMobile({ cityName, active_page, links }) {
             <div className="containerLegalMobile">
               <div className="line"></div>
               <div className="text">
-                <p>Мы{' '}
-                  <Link className="link" href={'/' + cityName + '/politika-legal'}>используем</Link>{' '}
+                <p>
+                  Мы{' '}
+                  <Link
+                    className="link"
+                    href={'/' + cityName + '/politika-legal'}
+                  >
+                    используем
+                  </Link>{' '}
                   файлы «Cookie» и метрическую систему «Яндекс.Метрика» для
                   сбора и анализа информации о производительности и
                   использовании сайта. Продолжая пользоваться сайтом, вы
@@ -247,4 +371,4 @@ export default function FooterMobile({ cityName, active_page, links }) {
       <ModalOrderMobile />
     </>
   );
-};
+}
--- modules/footer/footerPC.js
+++ modules/footer/footerPC.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import {useHomeStore} from '@/components/store';
+import { useHomeStore } from '@/components/store';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 import Link from 'next/link';
 import Typography from '@mui/material/Typography';
 
 import { NewVKIcon, OdnIcon, TGIcon, RutubeIcon, ArrowUp } from '@/ui/Icons.js';
-import { getLocalStorageItem, setLocalStorageItem } from '@/utils/browserStorage';
+import {
+  getLocalStorageItem,
+  setLocalStorageItem,
+} from '@/utils/browserStorage';
 import { BREAKPOINTS } from '@/utils/breakpoints';
 
 export default React.memo(function FooterPC({ cityName, active_page, links }) {
-
   const [cookie, setCookie] = useState(true);
   const [showArrow, setShowArrow] = useState(false);
 
-  const [badge_filter, tag_filter, text_filter] = useHomeStore((state) => [state.badge_filter, state.tag_filter, state.text_filter]);
+  const [badge_filter, tag_filter, text_filter] = useHomeStore((state) => [
+    state.badge_filter,
+    state.tag_filter,
+    state.text_filter,
+  ]);
   const isTabletFooter = useMediaQuery(
     `(min-width:${BREAKPOINTS.tabletMin}px) and (max-width:${BREAKPOINTS.tabletMax}px)`
   );
@@ -23,7 +29,8 @@ export default React.memo(function FooterPC({ cityName, active_page, links }) {
 
   const handlerArrow = () => setShowArrow(window.scrollY > 50);
 
-  const scrollUp = () => window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+  const scrollUp = () =>
+    window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
 
   const acceptCookie = () => {
     setLocalStorageItem('setCookie', true);
@@ -38,12 +45,15 @@ export default React.memo(function FooterPC({ cityName, active_page, links }) {
   useEffect(() => {
     const savedCookie = getLocalStorageItem('setCookie');
 
-    if (!savedCookie?.length){
+    if (!savedCookie?.length) {
       setCookie(false);
-    } 
+    }
   }, []);
 
-  const ext = (url) => url ? { href: url, target: '_blank', rel: 'noopener noreferrer' } : { href: '/#' };
+  const ext = (url) =>
+    url
+      ? { href: url, target: '_blank', rel: 'noopener noreferrer' }
+      : { href: '/#' };
 
   const footerStyle = {
     minHeight: isTabletFooter
@@ -53,33 +63,65 @@ export default React.memo(function FooterPC({ cityName, active_page, links }) {
       : cookie
         ? '36.101083032491vw'
         : '45.126353790614vw',
-    marginTop: active_page === 'contacts' ? null : isTabletFooter ? tabletVw(40) : '1.8050541516245vw',
+    marginTop:
+      active_page === 'contacts'
+        ? null
+        : isTabletFooter
+          ? tabletVw(40)
+          : '1.8050541516245vw',
   };
 
   return (
     <>
-      <div className={showArrow ? 'ArrowPC' : 'ArrowHidden'} onClick={scrollUp} style={{marginTop: active_page === 'sitemap' || active_page === 'contacts' || (active_page === 'home' && (badge_filter || tag_filter || text_filter)) ? '-4.3321299638989vw' : null, transform: active_page === 'contacts' ? 'translate(0, -50%)' : null}}>
+      <div
+        className={showArrow ? 'ArrowPC' : 'ArrowHidden'}
+        onClick={scrollUp}
+        style={{
+          marginTop:
+            active_page === 'sitemap' ||
+            active_page === 'contacts' ||
+            (active_page === 'home' &&
+              (badge_filter || tag_filter || text_filter))
+              ? '-4.3321299638989vw'
+              : null,
+          transform: active_page === 'contacts' ? 'translate(0, -50%)' : null,
+        }}
+      >
         <ArrowUp />
       </div>
-      <footer className={`footerPC ${active_page === 'contacts' ? 'footerPC--contacts' : ''}`} style={footerStyle}>
+      <footer
+        className={`footerPC ${active_page === 'contacts' ? 'footerPC--contacts' : ''}`}
+        style={footerStyle}
+      >
         <div className="ContainerPCFooter">
           <div className="column">
             <Typography component="span">Жако</Typography>
             <Link href={'/' + cityName + '/about'}>О Компании</Link>
             <Link href={'/' + cityName + '/company-details'}>Реквизиты</Link>
             <Link href={'/' + cityName + '/contacts'}>Контакты</Link>
+            <Link href={'/' + cityName + '/dostavka'}>Доставка</Link>
           </div>
           <div className="column">
             <Typography component="span">Документы</Typography>
-            {Object.keys(links || {}).length === 0 ? false :
+            {Object.keys(links || {}).length === 0 ? (
+              false
+            ) : (
               <Link {...ext(links?.link_allergens)}>
                 Калорийность, состав, БЖУ
               </Link>
-            }
-            <Link href={'/' + cityName + '/publichnaya-oferta'}>Публичная оферта</Link>
-            <Link href={'/' + cityName + '/politika-konfidencialnosti'}>Политика конфиденциальности</Link>
-            <Link href={'/' + cityName + '/legal'}>Согласие на обработку персональных данных</Link>
-            <Link href={'/' + cityName + '/politika-legal'}>Политика в отношении обработки метрических данных</Link>
+            )}
+            <Link href={'/' + cityName + '/publichnaya-oferta'}>
+              Публичная оферта
+            </Link>
+            <Link href={'/' + cityName + '/politika-konfidencialnosti'}>
+              Политика конфиденциальности
+            </Link>
+            <Link href={'/' + cityName + '/legal'}>
+              Согласие на обработку персональных данных
+            </Link>
+            <Link href={'/' + cityName + '/politika-legal'}>
+              Политика в отношении обработки метрических данных
+            </Link>
             <Link href={'/' + cityName + '/instpayorders'}>Правила оплаты</Link>
             <Link href={'/' + cityName + '/sitemap'}>Карта сайта</Link>
           </div>
@@ -89,16 +131,20 @@ export default React.memo(function FooterPC({ cityName, active_page, links }) {
           </div>
           <div className="column">
             <Typography component="span">Франшиза</Typography>
-            <Link  {...ext('https://franchise.jacofood.ru')}>
-              Сайт франшизы
-            </Link>
+            <Link {...ext('https://franchise.jacofood.ru')}>Сайт франшизы</Link>
             <Link {...ext('https://invest.jacofood.ru')}>
               Сайт для инвестиций
             </Link>
           </div>
           <div className="container">
-            <div className="icon" role="navigation" aria-label="Мы в социальных сетях">
-              {Object.keys(links || {}).length === 0 ? false :
+            <div
+              className="icon"
+              role="navigation"
+              aria-label="Мы в социальных сетях"
+            >
+              {Object.keys(links || {}).length === 0 ? (
+                false
+              ) : (
                 <>
                   <Link {...ext(links?.link_vk)} aria-label="Мы ВКонтакте">
                     <NewVKIcon aria-hidden="true" focusable="false" />
@@ -106,21 +152,29 @@ export default React.memo(function FooterPC({ cityName, active_page, links }) {
                   <Link {...ext(links?.link_tg)} aria-label="Мы в Telegram">
                     <TGIcon aria-hidden="true" focusable="false" />
                   </Link>
-                  <Link {...ext(links?.link_ok)} aria-label="Мы в Одноклассниках">
+                  <Link
+                    {...ext(links?.link_ok)}
+                    aria-label="Мы в Одноклассниках"
+                  >
                     <OdnIcon aria-hidden="true" focusable="false" />
                   </Link>
                   <Link {...ext(links?.link_rt)} aria-label="Мы в RuTube">
                     <RutubeIcon aria-hidden="true" focusable="false" />
                   </Link>
                 </>
-              }
+              )}
             </div>
-            
           </div>
         </div>
         <div className="ContainerCopyFooter">
           <div className="copy">
-            <Typography component="span" className="copy" suppressHydrationWarning>{new Date().getFullYear()} © Жако</Typography>
+            <Typography
+              component="span"
+              className="copy"
+              suppressHydrationWarning
+            >
+              {new Date().getFullYear()} © Жако
+            </Typography>
           </div>
         </div>
 
@@ -130,12 +184,17 @@ export default React.memo(function FooterPC({ cityName, active_page, links }) {
               <div className="text">
                 <p>
                   Мы{' '}
-                  <Link className="link" href={'/' + cityName + '/politika-legal'}>используем</Link>{' '}
-                  файлы «Cookie» и метрическую систему
-                  «Яндекс.Метрика» для сбора и анализа информации о
-                  производительности и использовании сайта. Продолжая пользоваться
-                  сайтом, вы соглашаетесь на размещение файлов «Cookie» и
-                  обработку данных метрических систем.
+                  <Link
+                    className="link"
+                    href={'/' + cityName + '/politika-legal'}
+                  >
+                    используем
+                  </Link>{' '}
+                  файлы «Cookie» и метрическую систему «Яндекс.Метрика» для
+                  сбора и анализа информации о производительности и
+                  использовании сайта. Продолжая пользоваться сайтом, вы
+                  соглашаетесь на размещение файлов «Cookie» и обработку данных
+                  метрических систем.
                 </p>
               </div>
               <div className="buttonAccept" onClick={acceptCookie}>
