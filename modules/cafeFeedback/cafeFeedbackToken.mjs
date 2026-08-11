275237a2f6716875b3b85044048ff4dcd50c5b80
tedgregory <mail@tedgregory.ru>
2026-08-11 12:19:31 +0300

    feat: shorten links, document flow

--- modules/cafeFeedback/cafeFeedbackToken.mjs
+++ modules/cafeFeedback/cafeFeedbackToken.mjs
@@ -1,4 +1,4 @@
-export const TOKEN_PATTERN = /^[a-z2-7]{32,2048}$/;
+export const TOKEN_PATTERN = /^(?:[a-z2-9]{8,16}|[a-z2-7]{32,2048})$/;
 
 export function isValidCafeFeedbackToken(token) {
   return typeof token === 'string' && TOKEN_PATTERN.test(token);
